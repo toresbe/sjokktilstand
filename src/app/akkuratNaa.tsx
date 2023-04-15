c9092463cb71967e28862d10ceff75a43f0463b3
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-04-16 01:47:25 +0200

    Improve marquee

--- src/app/akkuratNaa.tsx
+++ src/app/akkuratNaa.tsx
@@ -1,19 +1,34 @@
 "use client";
 import Marquee from "react-fast-marquee";
 
-const Tekst = () => <h1 className={"p-4 font-bold text-3xl"}>AKKURAT NÅ</h1>;
-
 export const AkkuratNaa = () => {
   return (
-    <Marquee speed={150} gradient={false} className={"h-20"}>
-      <Tekst />
-      <Tekst />
-      <Tekst />
-      <Tekst />
-      <Tekst />
-      <Tekst />
-      <Tekst />
-      <Tekst />
+    <Marquee
+      speed={100}
+      gradient={false}
+      className={"h-20 p-4 font-bold text-3xl"}
+    >
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
+      <span className={"pr-16"}>AKKURAT NÅ</span>
     </Marquee>
   );
 };
