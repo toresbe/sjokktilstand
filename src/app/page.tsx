d414efb687daa970de2a8af710a34895278bd762
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-04-17 01:51:24 +0200

    Sett max-height til 100dvh fremfor vh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const rapport = await shockReport();
 
   return (
-    <main className="flex min-h-screen flex-col lg:justify-center bg-black">
+    <main className="flex min-h-[100dvh] flex-col lg:justify-center bg-black">
       <div className={"bg-red text-white flex-col flex max-lg:grow"}>
         <AkkuratNaa />
         <SjokkSnitt rapport={rapport} />
