99888a849d726768dd16dafc3b295ac696961afa
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-04-17 02:27:27 +0200

    Center marquee

--- src/app/akkuratNaa.tsx
+++ src/app/akkuratNaa.tsx
@@ -3,9 +3,9 @@ import Marquee from "react-fast-marquee";
 
 export const AkkuratNaa = () => {
   return (
-    <div className={"h-14 p-4 font-bold text-2xl"}>
+    <div className={"h-14 font-bold text-2xl"}>
       <Marquee speed={90} gradient={false}>
-        <div className={"flex gap-8 pr-8"}>
+        <div className={"p-3 justify-center flex gap-8 pr-8"}>
           <div>AKKURAT NÅ</div>
           <div>AKKURAT NÅ</div>
           <div>AKKURAT NÅ</div>
