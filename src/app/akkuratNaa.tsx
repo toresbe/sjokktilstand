ee18d18fdc0929a3ae1aa31d4327913481741680
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-04-17 02:14:31 +0200

    Forhindre content shift ved load

--- src/app/akkuratNaa.tsx
+++ src/app/akkuratNaa.tsx
@@ -3,34 +3,32 @@ import Marquee from "react-fast-marquee";
 
 export const AkkuratNaa = () => {
   return (
-    <Marquee
-      speed={100}
-      gradient={false}
-      className={"h-14 p-4 font-bold text-2xl"}
-    >
-      <div className={"flex gap-8 pr-8"}>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-        <div>AKKURAT NÅ</div>
-      </div>
-    </Marquee>
+    <div className={"h-14 p-4 font-bold text-2xl"}>
+      <Marquee speed={90} gradient={false}>
+        <div className={"flex gap-8 pr-8"}>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+          <div>AKKURAT NÅ</div>
+        </div>
+      </Marquee>
+    </div>
   );
 };
