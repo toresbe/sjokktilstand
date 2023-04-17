13db911e333ab844891d0a82073fb14bc4d93ef8
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-04-17 02:16:15 +0200

    Refaktorer kode

--- src/app/SjokkSnitt.tsx
+++ src/app/SjokkSnitt.tsx
@@ -1,4 +1,4 @@
-import { ShockData } from "@/lib/shockReport";
+import { ShockData, ShockednessSample } from "@/lib/shockReport";
 import { format } from "date-fns";
 import { nb } from "date-fns/locale";
 
@@ -39,57 +39,69 @@ export const getRelativeShock = (
 };
 
 const RelativtSjokk = ({ level }: { level: ShockLevel }) => (
-  <>– Dagbladet er {sjokkLevels[level]} sjokkert</>
+  <h2 className={"text-xl sm:text-3xl"}>
+    – Dagbladet er {sjokkLevels[level]} sjokkert:
+  </h2>
 );
 
-export const SjokkSnitt = ({ rapport }: { rapport: ShockData }) => {
-  const { shocks, newest, avgShocks } = rapport;
+function SjokkListe({ newest }: { newest: ShockednessSample }) {
   return (
-    <div
-      className={
-        "p-4 lg:p-12 bg-red text-black bg-yellow flex flex-col items-center justify-center max-lg:grow"
-      }
-    >
-      <div className={"w-full max-w-xl space-y-5"}>
+    <div className={"flex flex-wrap gap-2"}>
+      {newest.sjokk
+        .sort((a, b) => b.length - a.length)
+        .map((x, i) => (
+          <div
+            key={i}
+            className={
+              "px-2 py-1 bg-red font-bold text-lg text-white whitespace-nowrap"
+            }
+          >
+            {x}
+          </div>
+        ))}
+    </div>
+  );
+}
+
+function SjokkTallene({
+  rapport: { newest, shocks, avgShocks },
+}: {
+  rapport: ShockData;
+}) {
+  return (
+    <div className={"space-y-2"}>
+      <h3 className={"text-2xl font-bold pb-1"}>Se sjokktallene:</h3>
+      <div>
+        Ordet «sjokk» forekommer {newest.sjokkCount === 1 ? "bare " : ""}
+        {newest.sjokkCount} {newest.sjokkCount === 1 ? "gang" : "ganger"} på
+        forsiden (per{" "}
+        {format(newest.timestamp, "dd. MMMM HH:mm", {
+          locale: nb,
+        })}
+        ).
+      </div>
+      <div>
+        Gjennomsnitt (av {shocks.length} målinger siden{" "}
+        {format(shocks[shocks.length - 1].timestamp, "dd. MMMM yyyy", {
+          locale: nb,
+        })}
+        ) er {avgShocks.toFixed(2)}.
+      </div>
+    </div>
+  );
+}
+
+export const SjokkSnitt = ({ rapport }: { rapport: ShockData }) => {
+  const { newest, avgShocks } = rapport;
+  return (
+    <div className={"bg-yellow text-black max-lg:flex-grow"}>
+      <div
+        className={"lg:mx-auto w-full max-w-2xl p-4 lg:p-12 h-max space-y-4"}
+      >
         <h1 className={"text-3xl sm:text-5xl font-black"}>SJOKKRAPPORT:</h1>
-        <h2 className={"text-lg sm:text-3xl"}>
-          <RelativtSjokk
-            level={getRelativeShock(avgShocks, newest.sjokkCount)}
-          />
-        </h2>
-        <div className={"flex flex-wrap gap-2"}>
-          {newest.sjokk
-            .sort((a, b) => b.length - a.length)
-            .map((x, i) => (
-              <div
-                key={i}
-                className={"px-2 py-1 bg-red text-white whitespace-nowrap"}
-              >
-                {x}
-              </div>
-            ))}
-        </div>
-        <div className={"space-y-1"}>
-          <h3 className={"text-xl lg:text-2xl font-bold pb-1"}>
-            Se sjokktallene:
-          </h3>
-          <div>
-            Ordet «sjokk» forekommer {newest.sjokkCount === 1 ? "bare " : ""}
-            {newest.sjokkCount} {newest.sjokkCount === 1 ? "gang" : "ganger"} på
-            forsiden (per{" "}
-            {format(newest.timestamp, "dd. MMMM HH:mm", {
-              locale: nb,
-            })}
-            ).
-          </div>
-          <div>
-            Gjennomsnitt (av {shocks.length} målinger siden{" "}
-            {format(shocks[shocks.length - 1].timestamp, "dd. MMMM yyyy", {
-              locale: nb,
-            })}
-            ) er {avgShocks.toFixed(2)}.
-          </div>
-        </div>
+        <RelativtSjokk level={getRelativeShock(avgShocks, newest.sjokkCount)} />
+        <SjokkListe newest={newest} />
+        <SjokkTallene rapport={rapport} />
       </div>
     </div>
   );
