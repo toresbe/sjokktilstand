9ae9444cacb19c8e1d40a4be75672e676040a8d9
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-04-18 23:54:11 +0200

    the joke's run its course

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { AkkuratNaa } from "@/app/akkuratNaa";
 import { ShockPage } from "@/app/ShockPage";
 import { shockReport } from "@/lib/shockReport";
-import { SubjektKanTaSegEnBolle } from "@/app/akkuratTrans";
 
 export default async function Home() {
   const rapport = await shockReport();
@@ -11,7 +10,7 @@ export default async function Home() {
       <div className={"bg-red text-white flex-col flex max-lg:grow"}>
         <AkkuratNaa />
         <ShockPage rapport={rapport} />
-        <SubjektKanTaSegEnBolle />
+        <AkkuratNaa />
       </div>
     </main>
   );
